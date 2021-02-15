c5cebe41d7633533402a3e8ef440b0f06cccd0db
elderlr <56103810+elderlr@users.noreply.github.com>
2021-02-15 17:24:18 -0600

    Update appointment.js

--- appointment.js
+++ appointment.js
@@ -2,33 +2,24 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-    function getCustomers(res, mysql, context, complete){
-        mysql.pool.query("SELECT Customers.CustomerID, Customers.CusFirst, Customers.CusLast, Contacts.PhoneNum FROM Customers LEFT OUTER JOIN Contacts on Customers.CustomerID = Contacts.CustomerID", function(error, results, fields){
+    
+    function getAppointmentBlock(res, mysql, context, complete){
+        mysql.pool.query("SELECT Appointments.DateTime, function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.customers  = results;
+            context.appointments  = results;
             complete();
         });
     }
-
-
-
-   function getCustomer(res, mysql, context, id, complete){
-        var sql = "SELECT Customers.CustomerID, Customers.CusFirst, Customers.CusLast, Contacts.PhoneNum FROM Customers LEFT OUTER JOIN Contacts on Customers.CustomerID = Contacts.CustomerID WHERE Customers.Customerid = ?";
-        var inserts = [id];
-        mysql.pool.query(sql, inserts, function(error, results, fields){
+    function getAppointment(res, mysql, context, complete){
+        mysql.pool.query("SELECT Appointments.DateTime, Appointments.TreatmentID, Appointments.CustomerID, Appointments.Red, Appointments.Orange, Appointments.Yellow, Appointments.Green, Appointments.Blue, Appointments.Indigo, Appointments.Violet", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.customers = results[0];
+            context.appointments  = results;
             complete();
         });
     }
-
-    /* Find people whose fname starts with a given string in the req */
-    function getCusWithNameLike(req, res, mysql, context, complete) {
-        //sanitize the input as well as include the % character
-         var query = "SELECT Customers.CustomerID, Customers.CusFirst, Customers.CusLast FROM Customers WHERE Customers.CusFirst LIKE " + mysql.pool.escape(req.params.s + '%');
