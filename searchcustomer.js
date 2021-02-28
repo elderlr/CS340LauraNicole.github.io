0a23273fcd5b45a7ea76a7a2864657caad0656d4
elderlr <56103810+elderlr@users.noreply.github.com>
2021-02-27 20:27:49 -0600

    Update searchcustomer.js

--- searchcustomer.js
+++ searchcustomer.js
@@ -1,6 +1,6 @@
 function searchCusByFirstName() {
-    //get the first name 
-    var first_name_search_string  = document.getElementById('first_name_search_string').value
-    //construct the URL and redirect to it
-    window.location = 'customer/search/' + encodeURI(first_name_search_string)
-}
+    //get the first name
+        var first_name_search_string  = document.getElementById('first_name_search_string').value
+    //        //construct the URL and redirect to it
+                window.location = 'customer/search/' + encodeURI(first_name_search_string)
+                }
